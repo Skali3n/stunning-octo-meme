18e79e3712ffed26a428756fe3fa4b440293b82c
skali3n <darththror@gmail.com>
2023-10-29 13:50:50 +0200

    second commit

--- middleware/authGuard.js
+++ middleware/authGuard.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const secretKey = 'your-secret-key'; // Замените на свой секретный ключ
+const secretKey = 'HS256'; 
 
 function authGuard(req, res, next) {
   const token = req.headers.authorization;
